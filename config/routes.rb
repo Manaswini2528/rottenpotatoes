092632c5c08b13e54030bae2b56ae6b05f0f9516
manaswinivivek <manaswinivivek@tamu.edu>
2023-02-15 20:26:06 -0600

    pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+Rails.application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
+end
